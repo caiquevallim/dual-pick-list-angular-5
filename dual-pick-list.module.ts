95497b3eae5fbe39aa870da76f120bcc2fe2b82c
Caique Vallim <caiquevallim@hotmail.com>
2018-01-25 18:45:56 -0200

    Has been included the pipe DualPickListPipe inside of the dual-pick-list.module

--- dual-pick-list.module.ts
+++ dual-pick-list.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
+import {DualPickListPipe} from "./pipe/dual-pick-list.pipe";
 
 @NgModule({
   imports: [
+
   ],
   declarations: [
+    DualPickListPipe
   ],
   exports:[
+    DualPickListPipe
   ]
 })
 export class DualPickListModule { }
